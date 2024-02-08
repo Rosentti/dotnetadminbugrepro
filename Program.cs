ac5f880fd448bcccc0d386e773a9e7315e51be23
Rosentti <onni.kukkonen77@gmail.com>
2024-02-08 16:57:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Repro;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+		Console.WriteLine("The code in this block doesn't really matter");
+    }
+}
